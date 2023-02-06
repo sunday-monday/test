b7f102e6aef11e375f7940f16d6e45398c38542d
sunday-monday <124451217+sunday-monday@users.noreply.github.com>
2023-02-06 23:47:28 +0530

    Create without_safe.dart

---
+++ p2/without_safe.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+ runApp(MaterialApp(
+   home: Text(
+     'without safearea',
+     textAlign: TextAlign.center,
+     style: TextStyle(
+       fontSize: 50,
+       color: Colors.yellow
+     ),
+   ),
+ ));
+}
