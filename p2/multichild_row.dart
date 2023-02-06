a808a205ddcf41713aa490fb4ec7054fd139a705
sunday-monday <124451217+sunday-monday@users.noreply.github.com>
2023-02-06 23:51:36 +0530

    Create multichild_row.dart

---
+++ p2/multichild_row.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+ runApp(MaterialApp(
+   home:Scaffold(
+     body: RowApp(
+     ),
+     ),
+   ),);
+}
+
+class RowApp extends StatefulWidget {
+ const RowApp({Key? key}) : super(key: key);
+
+ @override
+ _RowAppState createState() => _RowAppState();
+}
+
+class _RowAppState extends State<RowApp> {
+ @override
+ Widget build(BuildContext context) {
+   return Container(
+     child:SafeArea(
+       child:Row(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children:[
+           Container(
+           height: 100,
+           width: 100,
+           color: Colors.blue, // change the color to red and green for other two
+
+             child: Center(
+               child: Text(
+                 'First Child'
+               ),
+             ),
+         ),
+           Container(
+             height: 100,
+             width: 100,
+             color: Colors.green, // change the color to red and green for other two
+             child: Center(
+               child: Text(
+                 'second Child'
+               ),
+           ),
+       ),
+             Container(
+               height: 100,
+               width: 100,
+               color: Colors.yellow, // change the color to red and green for other two
+               child: Center(
+                 child: Text(
+                   'third Child'
+                 ),
+             ),
+       ),
+         ],
+       ),// Color attribute for the inner container
+   ), );
+ }
+}
