e6f0ec39b0a086336102d7b2e2dae58fcd8cc5c2
sunday-monday <124451217+sunday-monday@users.noreply.github.com>
2023-02-06 20:50:44 +0530

    Update activity_life_cycle.java

--- p1/activity_life_cycle.java
+++ p1/activity_life_cycle.java
@@ -1,19 +1,58 @@
-package com.example.p3activitylifecycle;
+package com.example.junk1;
 
+import static android.widget.Toast.makeText;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
-
+    Button bt_p,bt_n;
+    ImageView iv;
+    boolean flag;
+    int images[] = {R.drawable._1, R.drawable._2,R.drawable._3,R.drawable._4};
+    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "ACTIVITY CREATED", Toast.LENGTH_SHORT).show();
+        Context context = getApplicationContext();
+        CharSequence text = "App just created ";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = makeText(context, text, duration);
+        toast.show();
+        iv = (ImageView) findViewById(R.id.img);
+        bt_p = (Button) findViewById(R.id.button_previous);
+        bt_n = (Button) findViewById(R.id.button_next);
+        flag = true;
+        bt_p.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i = (i-1) % images.length;
+                if(i<0)
+                    i += images.length;
+                iv.setImageResource(images[i]);
+            }
+        });
+        bt_n.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i = (i+1) % images.length;
+                iv.setImageResource(images[i]);
+            }
+        });
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://amazon.com");
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
     }
-
     @Override
     protected void onStart() {
         super.onStart();
